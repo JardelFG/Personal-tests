b8c6235b7d1be88e66e121f0f7bdbf38ba13deed
jardelFG <jardelfigueroa98@gmail.com>
2023-10-01 19:36:27 -0300

    css scroll-time

--- tests-in/vanilla/src/index.js
+++ tests-in/vanilla/src/index.js
@@ -18,19 +18,30 @@ function switchOverlay(){
 
 // nav scroll style
 document.addEventListener("DOMContentLoaded", function() {
-    var header = document.querySelector(".nav-padding");
+    var header = document.querySelector(".nav-padding")
   
     window.addEventListener("scroll", function() {
-      var scroll = window.scrollY || document.documentElement.scrollTop;
+      var scroll = window.scrollY || document.documentElement.scrollTop
   
       if (scroll >= 10) {
-        header.classList.remove("nav-padding");
-        header.classList.add("scroll-on");
+        header.classList.remove("nav-padding")
+        header.classList.add("scroll-on")
       } else {
-        header.classList.remove("scroll-on");
-        header.classList.add("nav-padding");
+        header.classList.remove("scroll-on")
+        header.classList.add("nav-padding")
       }
     });
   });
 
 
+ // Cerrar el popover al hacer clic en el botón de cierre
+
+const allBtn = document.querySelectorAll('.switch-popover')
+// const AllpopoverBoss = document.querySelectorAll('.popover-boss-info')
+allBtn.forEach(div=>{
+  div.addEventListener('click', ()=>{
+   const divId = div.id
+   console.log(divId)
+  })
+  console.log(allBtn)
+})
